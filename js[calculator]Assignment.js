53e47b31d7cf11ad48975ac653d3972c2dd1b696
FizaRafiqh <119665194+FizaRafiqh@users.noreply.github.com>
2022-12-10 16:22:46 +0530

    Add files via upload
    calculator function assignment

---
+++ js[calculator]Assignment.js
@@ -0,0 +1,14 @@
+console.log("Calculator Assignment");
+
+function calculator(num1,num2){
+    console.log("The sum is ", num1+(num2));
+    console.log("The difference is ", num1-num2);
+    console.log("The product is ", num1*num2);
+    console.log("The division is ", num1/num2);
+}
+
+// let num1 = prompt("enter number 1");
+// let num2 = prompt("enter number 2");
+
+// calculator(num1,num2);
+calculator(2,2);
